ec08d627903a40cf86d98b1d403b8156a7f22de6
Aman Rawat <aman.r1298@gmail.com>
2020-12-02 17:37:34 +0530

    integrated with database

--- main.py
+++ main.py
@@ -3,18 +3,19 @@ from modules.apriori import Apriori
 
 from modules.segmentation import segmentation
 from sample_plots import plotting_sample
-
-
+import sqlite3
+import pandas as pd
 
 # it will contain only retailor specific functions
 class  Retailor():
-    def __init__(self, seg=segmentation(), Apriori = Apriori()):
+    def __init__(self, seg=segmentation(), Apriori = Apriori() ):
         """
             loads respective class's object in Retailor class
         """
         # self.plotting = plotting_sample()
         self.seg = seg
         self.apriori = Apriori
+        self.database = sqlite3.connect('./database/new_data.db')
         
 
 
@@ -23,7 +24,8 @@ class  Retailor():
         use apriori class functions 
         """
         # print('apriori')
-        data = self.apriori.club()
+        df = pd.read_sql_query("select * from transactions_1", self.database)
+        data = self.apriori.club(df)
         print(data.head())
         # pass
 
@@ -36,14 +38,15 @@ class  Retailor():
         """
         plot 2 plots of customer segments after dividing customer in different segments using RFM technique
         """
-        self.seg.get_customer_segments()
+        df = pd.read_sql_query("select * from transactions_1", self.database)
+        self.seg.get_customer_segments(df)
 
 
 
 # this object is for example only
 R = Retailor()
-# R.customer_segments()
 R.clubbing()
+R.customer_segments()
 
 # R.forecast()
 # R.clubbing()
