bbad86ebdc457ad0df3dc5c62885fd5265fafe9e
akashraj98 <akashraj7713@gmail.com>
2020-12-06 13:35:17 +0530

    small change

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ import sqlite3
 import time
 import datetime
 import random
-print('__file__={0:<35} | __name__={1:<20} | __package__={2:<20}'.format(__file__,__name__,str(__package__)))
 
 import main
 # import pandas as pdS
